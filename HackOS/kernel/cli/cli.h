c78b70a4fd552961516ffdc365db3d5698200890
sibotindustries <sibotindustries@gmail.com>
2024-09-13 11:37:04 -0300

    Delete HackOS directory

--- HackOS/kernel/cli/cli.h
+++
@@ -1,8 +0,0 @@
-// cli.h
-#ifndef CLI_H
-#define CLI_H
-
-void cli_init(void);
-void cli_execute(void);
-
-#endif
--- HackOS/kernel/timer/timer.h
+++
@@ -1,7 +0,0 @@
-// timer.h
-#ifndef TIMER_H
-#define TIMER_H
-
-void timer_init(uint32_t frequency);
-
-#endif
